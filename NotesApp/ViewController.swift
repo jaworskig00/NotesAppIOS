acf092ee9130c2391bb8b3a8ca693b3963b7d960
Grzegorz Jaworski <g.jaworski@selleo.com>
2023-06-08 12:44:28 +0200

    Initial Commit

---
+++ NotesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotesApp
+//
+//  Created by Grzegorz Jaworski on 08/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
